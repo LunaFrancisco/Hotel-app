afbbd07adc21fd1b42370900e1b16ee93652a096
LunaFrancisco <francisco.luna@mail.udp.cl>
2021-12-09 16:05:02 -0300

    add registro por turno

--- backend/controllers/registro.js
+++ backend/controllers/registro.js
@@ -1,7 +1,11 @@
 const { response } = require("express");
+const sequelize = require("../database/database");
+const Balance = require("../models/balance");
 
 const Registro = require("../models/registro");
 const Usuario = require("../models/usuario");
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
 
 const getRegistros = async (req, res = response) => {
   try {
@@ -27,6 +31,52 @@ const getRegistros = async (req, res = response) => {
   }
 };
 
+const getRegistrosTurno = async (req, res = response) => {
+  try {
+
+    const getBalance = await Balance.findOne({
+      where:{
+        fecha:{ [Op.ne]: null} 
+      },
+      order:[['fecha','DESC']],
+    });
+    console.log(getBalance.fecha)
+
+
+    var startDate = getBalance.fecha
+    var endDate = new Date();
+    console.log(endDate)
+
+    const allRegistros = await Registro.findAll({
+      attributes: [
+        'id', 'id_servicio', 'id_habitacion', 'fecha', 'fecha_entrada', 'monto', 'observacion'
+      ],
+      include: [{
+        model: Usuario
+      }],
+      order: [['fecha', 'DESC']]
+    },
+    {
+      where:{
+        $and:[{ fecha: {gte: startDate} },
+          { fecha: {lt: endDate} }]
+      }
+    }
+    );
+    return res.json({
+      ok: true,
+      allRegistros,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.json({
+      ok: false,
+      msg: "Error contacte a administrador",
+    });
+  }
+};
+
 module.exports = {
-  getRegistros
+  getRegistros,
+  getRegistrosTurno
 };
